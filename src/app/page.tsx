322c8fbc4f5bd41937c0043b9fd0295bcc0a3c16
Bongani Masango <bkmasango26@gmail.com>
2025-03-05 10:23:03 +0200

    set up prisma and local postgres database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       {' '}
-      <h1>Welcome to my Nextjs App!</h1>
+      <h1 className="text-2xl font-bold">Welcome to my Nextjs App!</h1>
     </div>
   );
 }
